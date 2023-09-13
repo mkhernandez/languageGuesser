1dd982fcf16a60552d074641c22073bedecc5b94
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-13 18:40:12 -0500

    added colors package

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const langs = require('langs');
 const franc = require('franc');
+const colors = require('colors');
 const log = console.log;
 const str = process.argv[2];
 const code = franc(str);
 
 try {
     const language = langs.where("3", code);
-    log(language.name);
+    log(language.name.rainbow);
 } catch {
     log('Sorry not in the system.');
 }
